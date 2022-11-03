ce8a17c3928278fdb4df6bf10c6d15ccbeab22c7
CCCcrazy <ylincx@connect.ust.hk>
2022-11-03 14:08:01 +0800

    icon

--- app/src/main/java/com/example/hkust_jobguide/profileFragment.kt
+++ app/src/main/java/com/example/hkust_jobguide/profileFragment.kt
@@ -6,13 +6,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class settingFragment : Fragment() {
+class profileFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_setting, container, false)
+        return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 }
